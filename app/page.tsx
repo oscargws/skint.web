dfa78b8af4fe9d923b09df97e0d7177b4ef7d246
Oscar Watson-Smith <oscar.watsonsmith@vanta.com>
2025-03-03 19:40:25 +1100

    chore: v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,62 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex min-h-screen w-full items-center justify-center bg-neutral-100 p-4">
+      <div className="max-w-3xl text-center">
+        <div className="space-y-6">
+          <div className="mb-4">
+            <span className="inline-flex items-center rounded-full border border-blue-100 bg-blue-50/50 px-2 py-0.5 text-[10px] text-blue-600/80">
+              EARLY ACCESS
+            </span>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <h1 className="text-3xl font-normal leading-tight md:text-4xl">
+            Skint is an{" "}
+            <span className="inline-flex items-center">
+              <span className="mx-1 inline-flex h-7 w-7 items-center justify-center rounded-lg bg-purple-50 text-2xl leading-none">
+                ✨
+              </span>
+            </span>
+            AI powered
+            <br />
+            personal finance tool built
+            <br />
+            by Aussies{" "}
+            <span className="inline-flex items-center">
+              <Image
+                src="https://oscarg.ws/img/oscar.jpg"
+                alt="Oscar"
+                width={30}
+                height={30}
+                className="mx-1 overflow-hidden rounded-full shadow-[0_2px_8px_rgba(0,0,0,0.1)]"
+              />
+            </span>{" "}
+            that costs less than <br />a flat white{" "}
+            <span className="inline-flex translate-y-[1px] items-center">
+              <span className="mx-1 inline-flex h-7 w-7 items-center justify-center rounded-lg bg-amber-50 text-2xl leading-none">
+                ☕️
+              </span>
+            </span>
+            !
+          </h1>
+          {/* 
+          <div className="mx-auto w-full max-w-md">
+            <div className="flex rounded-[20px] ">
+              <Input
+                type="email"
+                placeholder="Enter your email address"
+                className="flex-1 space-2 bg-transparent gap-2 px-4 text-base text-gray-600 placeholder:text-gray-500 focus-visible:ring-0 focus-visible:ring-offset-0"
+              />
+              <Button className="rounded-md bg-black px-6 text-white hover:bg-black/90">
+                Join waitlist
+              </Button>
+            </div>
+          </div> */}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "oscarg.ws",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
