54876698076817862e164506e585eeefc736f067
Oscar Watson-Smith <oscar.watsonsmith@vanta.com>
2025-03-03 19:43:23 +1100

    chore: lint fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+// import { Input } from "@/components/ui/input";
+// import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
